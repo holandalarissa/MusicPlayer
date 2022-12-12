c712563072e52233146553cee11115ac983978cd
Larissa <95726877+holandalarissa@users.noreply.github.com>
2022-12-11 23:55:02 -0300

    Add files via upload

---
+++ fun-javascript-projects.com-master/fun-javascript-projects.com-master/Music Player/Music/script.js
@@ -0,0 +1,70 @@
+const songs = [
+  "All I Am - Dyalla",
+
+  "Auld Lang Syne - DJ Williams",
+
+  "God Rest Ye Merry Gentlmen - Dj Williams",
+
+  "Last Train North - TrackTribe",
+
+  "Mulholland - King Canyon",
+  
+  "Ringside - Dyalla",  
+];
+const player = document.getElementById("player");
+
+const createSongList = () => {
+  const list = document.createElement("ol");
+  for (let i = 0; i < songs.length; i++) {
+    const item = document.createElement("li");
+    item.appendChild(document.createTextNode(songs[i]));
+    list.appendChild(item);
+  }
+  return list;
+};
+
+
+const songList = document.getElementById("songList");
+songList.appendChild(createSongList());
+const links = document.querySelectorAll("li");
+for (const link of links) {
+  link.addEventListener("click", setSong);
+}
+
+function setSong(e) {
+  document.querySelector("#headphones").classList.remove("pulse");
+
+  const source = document.getElementById("source");
+  source.src = "songs/" + e.target.innerText;
+  document.getElementById(
+    "currentSong"
+  ).innerText = `Tocando Agora:  ${e.target.innerText}`;
+
+  player.load();
+  player.play();
+
+  document.querySelector("#headphones").classList.add("pulse");
+}
+
+function playAudio() {
+  if (player.readyState) {
+    player.play();
+  }
+}
+
+function pauseAudio() {
+  player.pause();
+}
+
+const slider = document.getElementById("volumeSlider");
+slider.oninput = function (e) {
+  const volume = e.target.value;
+  player.volume = volume;
+};
+
+function updateProgress() {
+  if (player.currentTime > 0) {
+    const progressBar = document.getElementById("progress");
+    progressBar.value = (player.currentTime / player.duration) * 100;
+}}
+
